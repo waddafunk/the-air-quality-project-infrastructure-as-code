cf683509bb48f9de4367e1ad9ade3e2e105d408a
waddafunk <jacopo.piccirillo@gmail.com>
2025-02-22 23:25:07 +0100

    output storage account key

--- terraform/modules/data-lake/outputs.tf
+++ terraform/modules/data-lake/outputs.tf
@@ -15,3 +15,8 @@ output "primary_dfs_endpoint" {
 output "containers" {
   value = [for container in azurerm_storage_container.containers : container.name]
 }
+
+output "storage_account_key" {
+  value     = azurerm_storage_account.storage.primary_access_key
+  sensitive = true
+}
